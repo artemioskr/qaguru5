6d4e55dfb278f372665471a638f8d549b978f715
Artemiy Skryabin <artemio.skr@gmail.com>
2021-02-16 01:21:33 +0700

    googleTest

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTest {
+
+    @Test
+    void findGoogleInGoogle() {
+        open("https://google.com");
+        $(byName("q")).setValue("google").pressEnter();
+
+        $(".rso").shouldHave(text("www.google.ru"));
+    }
+}
